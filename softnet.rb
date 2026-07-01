3cd358380e7c440af07cd82087970cd6ad4aade4
Brian <dev@bjl13.com>
2026-06-30 22:54:00 -0500

    Refactor Softnet formula for macOS installation

--- softnet.rb
+++ softnet.rb
@@ -6,17 +6,14 @@ class Softnet < Formula
   desc "Software networking with isolation for Tart"
   homepage "https://github.com/cirruslabs/softnet"
   version "0.19.0"
-
-
-  url "https://github.com/cirruslabs/softnet/releases/download/0.19.0/softnet.tar.gz"
-  sha256 "1612e1296834aae0b6389650c7c5190add1ee8d71474e328691e67679ecda53c"
-
-  define_method(:install) do
-    bin.install "softnet"
+  on_macos do
+    url "https://github.com/cirruslabs/softnet/releases/download/0.19.0/softnet.tar.gz"
+    sha256 "1612e1296834aae0b6389650c7c5190add1ee8d71474e328691e67679ecda53c"
+    depends_on macos: :sequoia
+    def install
+      bin.install "softnet"
+    end
   end
-
-  depends_on :macos => :sequoia
-
   def caveats
     <<~EOS
       See the Github repository for more information
